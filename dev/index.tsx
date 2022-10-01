0310fb6630c2ba744e092626cd9c797fd744237e
Supertigerr <Supertigerr@users.noreply.github.com>
2022-10-01 16:46:35 +0000

    Format

--- dev/index.tsx
+++ dev/index.tsx
@@ -3,40 +3,42 @@ import "./styles.css";
 
 import App from "./App";
 
-
 import { createRouter, useNamedRoute } from "../src";
 
 const Router = createRouter({
   routes: [
     {
-      name: 'Home',
-      path: '/',
-      element: () => <div>Home Page</div>
+      name: "Home",
+      path: "/",
+      element: () => <div>Home Page</div>,
     },
     {
-      path: '/app',
-      element: () => <MainApp/>,
+      path: "/app",
+      element: () => <MainApp />,
       routes: [
         {
-          name: 'App',
-          path: '/'
+          name: "App",
+          path: "/",
         },
         {
-          name: 'Users',
-          path: '/users/:userId'
-        }
-      ]
-    }
-  ]
-})
+          name: "Users",
+          path: "/users/:userId",
+        },
+      ],
+    },
+  ],
+});
 
 const MainApp = () => {
   const namedRoute = useNamedRoute();
-  return (
-    <div style="white-space: pre;">
-      {JSON.stringify(namedRoute(), null, 2)}
-    </div>
-  )
-}
+  return <div style="white-space: pre;">{JSON.stringify(namedRoute(), null, 2)}</div>;
+};
 
-render(() => <Router><App /></Router>, document.getElementById("root")!);
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById("root")!,
+);

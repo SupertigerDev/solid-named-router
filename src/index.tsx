6909c8365242a1caef78f975117806af1f4f1ade
Supertiger <saawalkhn@googlemail.com>
2022-10-09 15:08:36 +0100

    Export useParams and useQuery.

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,1 @@
-export { createRouter, navigate, Link, RouterView, useNamedRoute, Outlet } from "./components";
+export { createRouter, navigate, Link, RouterView, useNamedRoute, useParams, useQuery, Outlet } from "./components";
